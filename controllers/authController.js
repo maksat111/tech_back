40716c7300487801ec83900ecfb347241698909c
maksat111 <maksatakmyradow111@gmail.com>
2023-04-07 04:37:24 +0500

    users login

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,45 +2,22 @@ const jwt = require('jsonwebtoken');
 const Teacher = require('../models/teachers');
 const bcrypt = require('bcryptjs');
 
-const createTeacher = async (req, res) => {
+const Login = async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        const foundTeacher = await Teacher.findOne({ username });
+        const foundUser = await Teacher.findOne({ username });
 
-        if (foundTeacher) {
-            return res.status(200).json({ success: 0, msg: 'User already exists!' });
-        }
-
-        const newTeacher = await Teacher.create({
-            name: 'Test',
-            surname: 'Test',
-            username,
-            password: bcrypt.hashSync(password, 10),
-        });
-
-        res.status(200).json({ success: 1, data: { username, password } });
-    } catch (err) {
-        res.status(500).json({ success: 0, msg: err.message })
-    }
-}
-
-const teacherLogin = async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        const foundTeacher = await Teacher.findOne({ username });
-
-        if (foundTeacher && (await bcrypt.compare(password, foundTeacher.password))) {
+        if (foundUser && (await bcrypt.compare(password, foundUser.password))) {
             const token = jwt.sign(
-                { teacher_id: foundTeacher._id, username },
+                { user_id: foundUser._id, username },
                 process.env.TOKEN_KEY,
                 {
                     expiresIn: "15h",
                 }
             );
-            foundTeacher.token = token;
-            return res.status(200).json({ success: 1, data: foundTeacher });
+            foundUser.token = token;
+            return res.status(200).json({ success: 1, data: foundUser });
         }
 
         res.status(200).json({ success: 0, msg: 'Invalid username or password!' });
@@ -53,5 +30,4 @@ const teacherLogin = async (req, res) => {
 }
 
 
-exports.teacherLogin = teacherLogin;
-exports.createTeacher = createTeacher;
+exports.Login = Login;
