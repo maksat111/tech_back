490563251841252ec3b728688abd92686d667f97
maksat111 <maksatakmyradow111@gmail.com>
2023-04-07 06:56:25 +0500

    added authentication

--- router/routes.js
+++ router/routes.js
@@ -14,36 +14,40 @@ const newsController = require('../controllers/newsController');
 
 // -------------------------------------------- Section Routes --------------------------------------------- //
 router.get('/section/list', sectionsController.getSections);
-router.post('/admin/section/create', sectionsController.create);
-router.patch('/admin/section/update/:id', sectionsController.update);
-router.delete('/admin/section/delete/:id', sectionsController.deleteSection);
+router.post('/admin/section/create', auth, sectionsController.create);
+router.patch('/admin/section/update/:id', auth, sectionsController.update);
+router.delete('/admin/section/delete/:id', auth, sectionsController.deleteSection);
+router.get('/admin/section/list', auth, sectionsController.getSections);
 
 
 //---------------------------------------------- News Routes -------------------------------------------------- //
 router.get('/news/list', newsController.getNews);
 router.get('/news/list/:id', newsController.getNewsBySection);
 router.get('/news/detail/:id', newsController.getNewsDetail);
-router.post('/admin/news/create', newsController.createNews);
-router.patch('/admin/news/update/:id', newsController.updateNews);
-router.delete('/admin/news/delete/:id', newsController.deleteNews);
+router.post('/admin/news/create', auth, newsController.createNews);
+router.patch('/admin/news/update/:id', auth, newsController.updateNews);
+router.delete('/admin/news/delete/:id', auth, newsController.deleteNews);
+router.get('/admin/news/list', auth, newsController.getNews);
 
 //---------------------------------------------- Banners Routes ---------------------------------------------------- //
-router.get('/banner', bannersController.getBanner);
-router.post('/admin/banner/create', bannersController.createBanner);
-router.patch('/admin/banner/update/:id', bannersController.updateBanner);
-router.delete('/admin/banner/delete/:id', bannersController.deleteBanner);
+router.get('/banner/list', bannersController.getBanner);
+router.post('/admin/banner/create', auth, bannersController.createBanner);
+router.patch('/admin/banner/update/:id', auth, bannersController.updateBanner);
+router.delete('/admin/banner/delete/:id', auth, bannersController.deleteBanner);
+router.get('/admin/banner/list', auth, bannersController.getBanner);
 
 //---------------------------------------------- About Routes --------------------------------------------------- //
 router.get('/about', aboutController.getAboutContent);
-router.post('/admin/about/create', aboutController.createAboutContent);
-router.patch('/admin/about/update/:id', aboutController.updateAboutContent);
-router.delete('/admin/about/delete/:id', aboutController.deleteAboutContent);
+router.post('/admin/about/create', auth, aboutController.createAboutContent);
+router.patch('/admin/about/update/:id', auth, aboutController.updateAboutContent);
+router.delete('/admin/about/delete/:id', auth, aboutController.deleteAboutContent);
+router.get('/admin/about', auth, aboutController.getAboutContent);
 
 //---------------------------------------------- User Routes --------------------------------------------------- //
-router.get('/admin/user', usersController.getUser);
-router.post('/admin/user/create', usersController.createUser);
-router.patch('/admin/user/update/:id', usersController.updateUser);
-router.delete('/admin/user/delete/:id', usersController.deleteUser);
+router.get('/admin/user/list', auth, usersController.getUser);
+router.post('/admin/user/create', auth, usersController.createUser);
+router.patch('/admin/user/update/:id', auth, usersController.updateUser);
+router.delete('/admin/user/delete/:id', auth, usersController.deleteUser);
 
 //---------------------------------------------- Auth Routes --------------------------------------------------- //
 router.post('/admin/login', authController.Login);
